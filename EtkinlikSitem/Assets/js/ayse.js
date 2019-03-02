1f249d4c8b032c9e73923671119d518179b29c41
ayşe özer <ayse.ozeer96@gmail.com>
2019-03-02 20:16:02 +0300

    first commit

---
+++ EtkinlikSitem/Assets/js/ayse.js
@@ -0,0 +1,26 @@
+﻿//function Gizle()
+//        {
+//            var divYakala = document.getElementById('divID');
+//            divYakala.style.display = 'none';
+//            divYakala.style.visibility = 'visible';
+//        }
+//        function Goster() {
+//            var divYakala = document.getElementById('divID');
+//            divYakala.style.display = 'block';
+//divYakala.style.visibility = 'visible';
+//        }
+//// Material Select Initialization
+//$(document).ready(function() {
+//$('.mdb-select').materialSelect();
+//});
+//< !--For a regular waves
+    Waves.attach('.btn', ['waves-effect']);
+
+//< !--For a lighter version of waves-- >
+    Waves.attach('.btn', ['waves-light']);
+
+
+// Material Select Initialization
+$(document).ready(function () {
+    $('.mdb-select').materialSelect();
+});
---
+++ EtkinlikSitem/Assets/js/gallery.js
@@ -0,0 +1,47 @@
+﻿$(document).ready(function () {
+
+    //İmage - gallery 1 
+
+    $("#gallery-class1").click(function () {
+
+        $("#gallery").hide(1000);
+        $("#gallery-2").hide(1000);
+        $("#gallery-1").show(1000);
+    });
+    $("#turn-back-1").click(function () {
+
+        $("#gallery").show(1000);
+        $("#gallery-1").hide(1000);
+    });
+
+    //İmage - gallery 2
+
+    $("#gallery-class2").click(function () {
+
+        $("#gallery").hide(1000);
+        $("#gallery-1").hide(1000);
+        $("#gallery-2").show(1000);
+    });
+    $("#turn-back-2").click(function () {
+
+        $("#gallery").show(1000);
+        $("#gallery-2").hide(1000);
+    });
+
+    //İmage - gallery 3
+
+    $("#gallery-class3").click(function () {
+
+        $("#gallery").hide(1000);
+        $("#gallery-1").hide(1000);
+        $("#gallery-2").hide(1000);
+        $("#gallery-3").show(1000);
+    });
+    $("#turn-back-3").click(function () {
+
+        $("#gallery").show(1000);
+        $("#gallery-3").hide(1000);
+    });
+
+
+});
